9996e80300f1b841920fa676e98c1ce5ae033e75
thelastmedici <christianopeyemijoshua@gmail.com>
2024-08-24 07:01:26 +0100

    added solution to task 4 and 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+--- Create a trigger that sets the valid_email column to 0 when the email column is updated.
+DELIMITER $$ ;
+CREATE TRIGGER resets_valid_email BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END;
+DELIMITER ;
